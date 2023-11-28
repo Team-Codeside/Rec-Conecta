f4ee2ea407004a7cd6966e8cc5da2c5c33df3e35
Gabriela Farias <gmffarias2004@gmail.com>
2023-11-28 17:26:21 -0300

    create componente carousel

---
+++ src/components/carousels/carousel.jsx
@@ -0,0 +1,33 @@
+import Carousel from 'react-bootstrap/Carousel';
+
+function Carousels() {
+  return (
+    <Carousel>
+      <Carousel.Item interval={1000}>
+        <img src="./Recife.jpg" alt="" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={500}>
+      <img src="./Recife.jpg" alt="" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img src="./Recife.jpg" alt="" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default Carousels;
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -4,8 +4,7 @@ import Footer from "../../components/footer/footer";
 import Input_control from "../../components/input/input";
 import Button_ from "../../components/button/button";
 import { useState } from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import Carousels from "../../components/carousels/carousel";
 
 const Home = () => {
     return (
@@ -50,36 +49,12 @@ const Home = () => {
                             <p>Descubra os melhores eventos da sua região</p>
                         </div>
                         <div className="eventos-caroussel">
-                            <Carousel activeIndex={index} onSelect={handleSelect}>
-                                <Carousel.Item>
-                                    <ExampleCarouselImage text="First slide" />
-                                    <Carousel.Caption>
-                                        <h3>First slide label</h3>
-                                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <ExampleCarouselImage text="Second slide" />
-                                    <Carousel.Caption>
-                                        <h3>Second slide label</h3>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <ExampleCarouselImage text="Third slide" />
-                                    <Carousel.Caption>
-                                        <h3>Third slide label</h3>
-                                        <p>
-                                            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-                                        </p>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                            </Carousel>
+                           
                         </div>
                     </div>
                 </section>
                 <section className="home-faq">
-
+                    <Carousels/>
                 </section>
             </main>
             <Footer />
