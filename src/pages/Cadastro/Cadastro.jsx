ed63b9042d1ea505e39876a52b084a6c2b03a540
Gabriela Farias <gmffarias2004@gmail.com>
2023-11-30 02:41:45 -0300

    update register

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,65 +1,66 @@
-import React from "react";
 import Input from "../../components/input/input";
 import Button from "../../components/button/button";
 import "../Cadastro/Cadastro.css";
 
 const Cadastro = () => (
   <>
-    <div class="container">
-     
-        <div className="form-image">
-          <img src="./bikes-cad.svg" alt="" /></div>
+    <div className="container-cadastro">
 
-      <div class="form">
+      <div className="form-image-cadastro">
+        {/* <img src="./bikes-cad.svg" alt=""/> */}
+      </div>
+
+      <div className="form-cadastro">
         <form action="#">
-          <div class="form-header">
-          <h1><b>Cadastre-se</b></h1>
+
+          <div className="form-header-cadastro">
+            <h1><b>Cadastre-se</b></h1>
+            <span />
           </div>
 
+          <div className="input-info-site-cadastro">
+            <div className="informacoes">
+              <b>
+                <h3><b>Infomaçoes</b></h3>
+              </b>
+            </div>
+            <div className="input-group">
+              <div className="input-box">
+                <Input
+                  type_="text"
+                  label_=""
+                  name_=""
+                  id_=""
+                  placeholder_="Nome completo"
+                  width_="850px"
+                  heigth_="40px"
+                  backgroundcolor_="#F7F7F7"
+                  color_=""
+                  border_="none"
+                  radius_="8px"
+                  fontsizelabel_=""
+                />
+              </div>
+              <div className="input-box">
+                <Input
+                  type_="email"
+                  label_=""
+                  name_=""
+                  id_=""
+                  placeholder_="Email"
+                  width_="850px"
+                  heigth_="40px"
+                  backgroundcolor_="#F7F7F7"
+                  color_=""
+                  border_="none"
+                  radius_="8px"
+                  fontsizelabel_=""
+                />
+              </div>
 
-
-          <div className="input-info-site">
-                <div className="informacoes">
-                    <b>
-                      <h3><b>Infomaçoes</b></h3>
-                    </b>
-                </div>
-              <div class="input-group">
-                <div class="input-box">
+              <div className="wrapper-inputs-box-cadastro">
+                <div className="input-box">
                   <Input
-                    type_="text"
-                    label_=""
-                    name_=""
-                    id_=""
-                    placeholder_="Nome completo"
-                    width_="850px"
-                    heigth_="40px"
-                    backgroundcolor_="#F7F7F7"
-                    color_=""
-                    border_="none"
-                    radius_="8px"
-                    fontsizelabel_=""
-                  />
-                </div>
-                <div class="input-box">
-                <Input
-                    type_="email"
-                    label_=""
-                    name_=""
-                    id_=""
-                    placeholder_="Email"
-                    width_="850px"
-                    heigth_="40px"
-                    backgroundcolor_="#F7F7F7"
-                    color_=""
-                    border_="none"
-                    radius_="8px"
-                    fontsizelabel_=""
-                  />
-                </div>
-                
-                <div class="input-box">
-                <Input
                     type_="password"
                     label_=""
                     name_=""
@@ -72,11 +73,11 @@ const Cadastro = () => (
                     border_="none"
                     radius_="8px"
                     fontsizelabel_=""
-                    
+
                   />
                 </div>
-                <div class="input-box">
-                <Input
+                <div className="input-box">
+                  <Input
                     type_="password"
                     label_=""
                     name_=""
@@ -92,73 +93,74 @@ const Cadastro = () => (
                   />
                 </div>
               </div>
+            </div>
           </div>
 
-          <div className="input-dados-pessoais">
+          <div className="input-dados-pessoais-cadastro">
             <div className="informacoes">
-              
-                <h3><b>Dados pessoais</b></h3>
-              
+
+              <h3><b>Dados pessoais</b></h3>
+
             </div>
-            <div class="input-group">
-                <div class="input-box">
+            <div className="input-group-cadastro">
+              <div className="input-box">
                 <Input
-                    type_="text"
-                    label_=""
-                    name_=""
-                    id_=""
-                    placeholder_="CPF"
-                    width_="390px"
-                    heigth_="40px"
-                    backgroundcolor_="#F7F7F7"
-                    color_=""
-                    border_="none"
-                    radius_="8px"
-                    fontsizelabel_=""
-                  />
-                </div>
-                <div class="input-box">
+                  type_="text"
+                  label_=""
+                  name_=""
+                  id_=""
+                  placeholder_="CPF"
+                  width_="390px"
+                  heigth_="40px"
+                  backgroundcolor_="#F7F7F7"
+                  color_=""
+                  border_="none"
+                  radius_="8px"
+                  fontsizelabel_=""
+                />
+              </div>
+              <div className="input-box">
                 <Input
-                    type_="date"
-                    label_=""
-                    name_=""
-                    id_=""
-                    placeholder_="Data de nascimento"
-                    width_="430px"
-                    heigth_="40px"
-                    backgroundcolor_="#F7F7F7"
-                    color_="#000"
-                    border_="none"
-                    radius_="8px"
-                    fontsizelabel_=""
-                    margin_= "0px 0px 0px 25px"
-                  />
-                </div>
-                <div />
+                  type_="date"
+                  label_=""
+                  name_=""
+                  id_=""
+                  placeholder_="Data de nascimento"
+                  width_="430px"
+                  heigth_="40px"
+                  backgroundcolor_="#F7F7F7"
+                  color_="#000"
+                  border_="none"
+                  radius_="8px"
+                  fontsizelabel_=""
+  
+                />
+              </div>
+              <div />
             </div>
 
           </div>
-              
-           
-          <div class="continue-button">
-                <Button
-                id_=""
-                className =""
-                width_="300px"
-                heigth_=""
-                backgroundcolor_="#FF5212"
-                color_="#fff"
-                border_="none"
-                radius_="8px"
-                weigth_="600"
-                font_="Roboto"
-                text_button="Confirmar"
-                padding_=""
-                margin_="30px auto"
-                link_="#"
-                type_="submit"
-                value_="Submit"
-                />
+
+
+          <div className="continue-button-cadastro">
+            <Button
+              id_=""
+              className=""
+              width_="300px"
+              heigth_=""
+              backgroundcolor_="#FF5212"
+              color_="#fff"
+              border_="none"
+              radius_="8px"
+              weigth_="600"
+              font_="Roboto"
+              text_button="Confirmar"
+              padding_=""
+              margin_="30px auto"
+              link_="#"
+              type_="submit"
+              value_="Submit"
+            />
           </div>
         </form>
       </div>
