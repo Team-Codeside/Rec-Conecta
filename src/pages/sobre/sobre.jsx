2082afee77616810e39273d65ab7c90e78a6497a
selma <selmalira00@gmail.com>
2023-11-30 22:23:32 -0300

    sobre nos--page

--- src/pages/sobre/sobre.jsx
+++ src/pages/sobre/sobre.jsx
@@ -40,46 +40,38 @@ const Sobre = () => {
                     <div id='container-como2-sobre'>
 
                         <div className='img-blocos-sobre'>
+
                             <div className='vector-sobre'>
-                                <div className='p-quadrado1-sobre'>
+                                <div className='p-pessoa-sobre'>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing <br /> elit. Iste unde nobis iure quasi.</p>
                                 </div>
                                 <img src="pessoa-item.png" alt="" />
                             </div>
                             <div className='vector-sobre'>
                                 <img src="Bussula-item.png" alt="" />
-                                <div className='p-quadrado2-sobre'>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing <br /> elit. Iste unde nobis iure quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa voluptas, eum architecto sunt culpa eligendi animi a similique, eaque esse debitis</p>
+                                <div className='p-bussula-sobre'>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste unde nobis iure quasi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
                                     
                                 </div>
                             </div>
                             <div className='vector-sobre'>
-                                <div className='p-quadrado3-sobre'>
+                                <div className='p-diagram-sobre'>
                                     <p>Lorem ipsum dolor sit amet consectetur adipisicing <br /> elit. Iste unde nobis iure quasi.  </p>
                                 </div>
                                 <img src="diagram-item.png" alt="" />
                             </div>
-                            {/* <div>
-                                <img src="diagrama-item.png" alt="" />
 
-                            </div>
-                            <div className='vector-sobre'>
-                                <img src="diagram-item.png" alt="" />
-                                <div className='p-quadrado4-sobre'>
-                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Iste unde nobis iure quasi.</p>
-                                </div>
-                            </div> */}
                         </div>
                     </div>
 
                 </div>
 
                 <div>
-                    <div className='linha-azul-sobre'>
+                    <div className='linha-azul-equipe-sobre'>
                         <img src="Rectangle 8-linha.svg" alt="" />
                     </div>
 
-                    <div className='sobre-texto3-sobre'>
+                    <div className='texto3-saude-sobre'>
                         <h2>Melhora de saúde mental para você</h2>
                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur eum ducimus ab consequatur <br /> asperiores minima. Sapiente adipisci deserunt temporibus laboriosam, fuga officia, deleniti voluptate, <br /> eum dignissimos cupiditate quam accusamus ipsam!</p>
                     </div>
@@ -88,7 +80,7 @@ const Sobre = () => {
             </main>
             <div>
                 <div className='img-video-sobre'><img src="video-sobre.svg" alt="" />
-                    <div className='p-video-sobre'>
+                    <div className='p-do-video-sobre'>
                         <p>Video informativo sobre o projeto</p>
                     </div>
                 </div>
@@ -96,19 +88,20 @@ const Sobre = () => {
 
 
             <div>
-                <div className='titulo-sobre-sobre'>
+                <div className='titulo-equipe-sobre'>
                     <h2>CONHEÇA NOSSA EQUIPE</h2>
 
                 </div>
             </div>
-            <div className='all-sobre'>
-                <div className='container-sobre'>
+            <div className='box-sobre'>
+                <div className='container-image-sobre'>
                     <div className='galeria-sobre'>
+
                         <div className="img-box-sobre"><h3>André Pinheiro</h3></div>
                         <div className="img-box-sobre"><h3>Aline Batista</h3></div>
                         <div className="img-box-sobre"><h3>Camilly Maria</h3></div>
                         <div className="img-box-sobre"><h3>Gabriela Farias</h3></div>
-                        <div className="img-box-sobre"><h3>Filipe Pereira</h3></div>
+                        {/* <div className="img-box-sobre"><h3>Filipe Pereira</h3></div> */}
 
                     </div>
                 </div>
