64f11f062e05a7cea4d3210e6c13f23348b0cb14
Andrepinheiroo <andrepinheiro.code@gmail.com>
2023-12-05 18:23:49 -0300

    fix perfil e eventos

--- src/pages/Evento/Evento.jsx
+++ src/pages/Evento/Evento.jsx
@@ -14,99 +14,102 @@ import { useNavigate } from "react-router-dom";
 const Eventos = () => {
 
 
-const [evento, setEvento] = useState({})
-  const { id } = useParams()
-  const { setFlashMessage } = useFlashMessage()
-  const [token] = useState(localStorage.getItem('token') || '')
-  const navigate = useNavigate();
+    const [evento, setEvento] = useState({})
+    const { id } = useParams()
+    const { setFlashMessage } = useFlashMessage()
+    const [token] = useState(localStorage.getItem('token') || '')
+    const navigate = useNavigate();
 
-  useEffect(() => {
-    api.get(`/eventos/${id}`).then((response) => {
-      setEvento(response.data.evento)
-    })
-  }, [id])
-  
-  async function Enviarinscricao() { //disparo inscrição
-    let msgType = 'success'
+    useEffect(() => {
+        api.get(`/eventos/${id}`).then((response) => {
+            setEvento(response.data.evento)
+        })
+    }, [id])
 
-    const data = await api.patch(`eventos/inscription/${evento._id}`,{
-        headers: {
-          Authorization: `Bearer ${JSON.parse(token)}`,
-        },
-      })
-      .then((response) => {
-        console.log(response.data)
-        return response.data
-      })
-      .catch((err) => {
-        console.log(err)
-        msgType = 'error'
-        return err.response.data
-      })
-     
+    async function Enviarinscricao() { //disparo inscrição
+        let msgType = 'success'
 
-    setFlashMessage(data.message, msgType)
-    navigate('/')
-    
-  }
+        const data = await api.patch(`eventos/inscription/${evento._id}`, {
+            headers: {
+                Authorization: `Bearer ${JSON.parse(token)}`,
+            },
+        })
+            .then((response) => {
+                console.log(response.data)
+                return response.data
+            })
+            .catch((err) => {
+                console.log(err)
+                msgType = 'error'
+                return err.response.data
+            })
+
+
+        setFlashMessage(data.message, msgType)
+        navigate('/')
+
+    }
     return (
         <>
-             <Navbar_ /> 
+            <Navbar_ />
             <div className="container-eventos">
                 <div className="header-eventos" >
-                
                     <img src={`http://localhost:5000/images/eventos/${evento.images}`} />
                 </div>
 
                 <div className="info-eventos">
                     <div className='titulo-info-eventos'>
                         <h1>Informações do evento</h1>
-                {token ? (
-                        <Button_
-                            width_="150px"
-                            heigth_="40px"
-                            backgroundcolor_="#FF5212"
-                            color_="#fff"
-                            border_="none"
-                            radius_=""
-                            weigth_="bold"
-                            font_="Roboto"
-                            text_button="Inscrever-se"
-                            padding_=""
-                            margin_=""
-                            type_="submit"
-                            value_="inscricao"
-                            Onclick_={Enviarinscricao}
-                        />) 
-                        :(
-                            
+                        {token ? (
                             <Button_
-                            width_="150px"
-                            heigth_="40px"
-                            backgroundcolor_="#FF5212"
-                            color_="#fff"
-                            border_="none"
-                            radius_=""
-                            weigth_="bold"
-                            font_="Roboto"
-                            text_button="Login"
-                            padding_=""
-                            margin_=""
-                            link_="/Login"
-                        />
-                        )}
+                                width_="150px"
+                                heigth_="40px"
+                                backgroundcolor_="#FF5212"
+                                color_="#fff"
+                                border_="none"
+                                radius_=""
+                                weigth_="bold"
+                                font_="Roboto"
+                                text_button="Inscrever-se"
+                                padding_=""
+                                margin_=""
+                                type_="submit"
+                                value_="inscricao"
+                                Onclick_={Enviarinscricao}
+                            />)
+                            : (
+
+                                <Button_
+                                    width_="150px"
+                                    heigth_="40px"
+                                    backgroundcolor_="#FF5212"
+                                    color_="#fff"
+                                    border_="none"
+                                    radius_=""
+                                    weigth_="bold"
+                                    font_="Roboto"
+                                    text_button="Login"
+                                    padding_=""
+                                    margin_=""
+                                    link_="/Login"
+                                />
+                            )}
                     </div>
                     <div className="detalhes-info-eventos">
                         <div className='container-detalhes-eventos'>
-                            <div className='box-input-eventos'>
+                            <div className='box-input-eventos evento-title'>
                                 <label htmlFor=""></label>
-                              
-                                <h1>{evento.name}</h1>
-                               
+
+                                <div className="title-evento">
+                                    <h1>{evento.name}</h1>
+                                </div>
+                                <div className="evento-linha-divisoria">
+                                    <img src="/linha.svg" alt="" />
+                                </div>
                             </div>
                             <div className='box-input-eventos'>
                                 <label htmlFor=""><em>Categoria</em></label>
-                               <p>{evento.categoria}</p>
+                                <p>{evento.categoria}</p>
                             </div>
                             <div className='box-input-eventos'>
                                 <label htmlFor="">Localização</label>
@@ -115,7 +118,6 @@ const [evento, setEvento] = useState({})
                             <div className='box-input-eventos'>
                                 <label htmlFor="">Inicio do evento</label>
                                 <p>{evento.dataev}</p>
-                    
                             </div>
 
 
@@ -127,7 +129,6 @@ const [evento, setEvento] = useState({})
                             <div className='box-input-eventos'>
                                 <label htmlFor="">Sobre o Evento</label>
                                 <p>{evento.description}</p>
-                                
                             </div>
                         </div>
                     </div>
@@ -135,24 +136,24 @@ const [evento, setEvento] = useState({})
                 </div>
 
                 <div className='container-organizador-eventos'>
-                     
+
                     <div className="organizador-eventos">
 
                         <div className="img-organizador">
                             <h1>ORGANIZADOR&#40;a&#41;</h1>
-                            <img src="./Rec.png" alt="" />
+                            <img src="/Rec.png" alt="" />
                         </div>
                         <div className="organizador-info">
                             <div className='box-info-organizador'>
                                 <label htmlFor="">Nome do Organizador&#40;a&#41;:</label>
-                               
+                                <p>Andrei Camila Santos</p>
                             </div>
                             <div className='box-info-organizador'>
                                 <label htmlFor="">Contatos:</label>
-                                <p>{evento.description}</p>
+                                <p></p>
                                 <p>
-                                andreia_camila_santos@hoatmail.com <br />   
-                                (96) 99719-2403
+                                    andreia_camila_santos@hoatmail.com <br />
+                                    (96) 99719-2403
                                 </p>
                             </div>
                         </div>
@@ -161,7 +162,7 @@ const [evento, setEvento] = useState({})
 
 
             </div>
-            <Footer />               
+            <Footer />
         </>
     );
 }
